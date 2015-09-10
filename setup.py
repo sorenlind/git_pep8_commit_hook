2c74c4428746a178dc1da47921f3b46822e75325
Søren Lind Kristiansen <soren@gutsandglory.dk>
2015-09-10 22:21:03 +0200

    Add compatibility info to setup.py.

--- setup.py
+++ setup.py
@@ -47,7 +47,8 @@ setup(
         "Environment :: Console",
         "License :: OSI Approved :: Apache License, Version 2.0",
         "Operating System :: OS Independent",
-        "Programming Language :: Python"
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
     ],
     entry_points={
         "console_scripts": [
